b12d1fd58ca2c3393ed65d780d9bd318e1473465
İSKENDER UZUN <uzuniskender@gmail.com>
2026-05-15 10:11:40 +0300

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -23,3 +23,13 @@ export * from "./loadAllStores"
 export const reloadTablesDispatched = () => {
   console.log("reloadTablesDispatched tetiklendi")
 }
+// Legacy support (eski kod kırılmasın diye)
+import { useOrderStore } from "./useOrderStore"
+import { useProductionStore } from "./useProductionStore"
+import { useWarehouseStore } from "./useWarehouseStore"
+
+export const useStore = () => ({
+  ...useOrderStore(),
+  ...useProductionStore(),
+  ...useWarehouseStore(),
+})
